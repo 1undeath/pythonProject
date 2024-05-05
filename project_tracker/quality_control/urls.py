b65bad3b93ffefe4b139e0681e9aff129d865e18
1undeath <nona.t34@yandex.ru>
2024-05-05 23:51:06 +0300

    def_str

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),  # Добавлено имя 'index'
     path('bug_list/', views.bug_list, name='bug_list'),  # Добавлено имя 'another_page'
     path('feature_list/', views.feature_list, name='feature_list'),  # Добавлено имя 'another_page'
+    path('bug_detail/<int:bug_id>/', views.bug_detail, name='bug_detail'),  # Добавлено имя 'another_page'
+    path('feature_detail/<int:feature_id>/', views.feature_detail, name='feature_detail'),  # Добавлено имя 'another_page'
 ]
